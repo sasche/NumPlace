1dc0c1f6c3bf6a8cddddeafd3b0f396001cc4296
Gumanatee <manatee0109@gmail.com>
2017-10-14 17:39:49 +0900

    座標情報保持、ボタン押下時セルに数字格納処理追加

--- src/assets/js/NumberPlace.js
+++ src/assets/js/NumberPlace.js
@@ -1,28 +1,55 @@
 /**
- * 
- */ 
+ *
+ */
+	var xAddress = null;
+	var yAddress = null;
+	var targetNum;
+
 	function init() {
 		for(var i=1; i<10; i++){
 			$("#numtable").append("<tr id=" + i + ">");
 			for(var j=1; j<10; j++){
-				$("#" + i).append("<td id=" + i + "_" + j + ">");
+				$("#" + i).append("<td class=" + j + ">");
 			}
 		}
+		$("#numtable").css("background-color","#ffffff");
+
 		for(var i=1; i<10; i++){
 			$(".SelNum").append("<input type = button value=" + i + " id=" + i + " class=numbutton>");
 		}
 		setEvent();
 	}
-	
+
 	function setEvent(){
 		$("#numtable").hover(
 			function(){
-				$(this).css("cursor","pointer");	
+				$(this).css("cursor","pointer");
 			}
 		);
 		$(".numbutton").hover(
 				function(){
-					$(this).css("cursor","pointer");	
+					$(this).css("cursor","pointer");
 				}
-			);	
+		);
+
+		$('body').on('click','td', function(){
+					xAddress = $(this).attr("class");
+					yAddress = $(this).parent().attr("id");
+					//console.log(xAddress + " " + yAddress);
+					$('td').css('background-color', 'transparent');
+					$(this).css("background-color","#7fffd4");
+				}
+		);
+
+		$(".SelNum").on('click', '.numbutton', function(){
+					targetNum = this.value;
+					console.log(targetNum);
+
+					if( xAddress != null && yAddress != null ){
+						$("#" + yAddress).find("." + xAddress).text(targetNum);
+						xAddress = null;
+						yAddress = null;
+					};
+				}
+		);
 	}
