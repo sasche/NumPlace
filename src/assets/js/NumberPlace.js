16293c5d4ec2a2a2c284a1e60c727c59a452da0d
baseo111 <baseo111@yahoo.co.jp>
2017-10-14 09:39:56 +0900

    マウスオーバーイベント等設定

--- src/assets/js/NumberPlace.js
+++ src/assets/js/NumberPlace.js
@@ -9,6 +9,20 @@
 			}
 		}
 		for(var i=1; i<10; i++){
-			$(".SelNum").append("<span>" + i);
+			$(".SelNum").append("<input type = button value=" + i + " id=" + i + " class=numbutton>");
 		}
+		setEvent();
+	}
+	
+	function setEvent(){
+		$("#numtable").hover(
+			function(){
+				$(this).css("cursor","pointer");	
+			}
+		);
+		$(".numbutton").hover(
+				function(){
+					$(this).css("cursor","pointer");	
+				}
+			);	
 	}
