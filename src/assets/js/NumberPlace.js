88de464e282cb51d3f8542ab98db4ccd7a835c25
阿部真也 <@DESKTOP-L3AGQ9B>
2017-10-17 23:49:37 +0900

    削除機能、チェック機能を雑に実装　※クリア時動作未確認

--- src/assets/js/NumberPlace.js
+++ src/assets/js/NumberPlace.js
@@ -4,6 +4,7 @@
 	var xAddress = null;
 	var yAddress = null;
 	var targetNum;
+	var successFlg = false;
 
 	function init() {
 		for(var i=1; i<10; i++){
@@ -12,8 +13,6 @@
 				$("#" + i).append("<td class=" + j + ">");
 			}
 		}
-		//$("#numtable").css("background-color","#ffffff");
-		//$("#1").find(".1").addClass("testing");
 
 		for(var i=1; i<10; i++){
 			$(".SelNum").append("<input type = button value=" + i + " class=numbutton>");
@@ -49,13 +48,92 @@
 
 					if( xAddress != null && yAddress != null ){
 						$("#" + yAddress).find("." + xAddress).text(targetNum);
-						//xAddress = null;
-						//yAddress = null;
 					};
 				}
 		);
+		$("#del").click(function(){
+			$("#" + yAddress).find("." + xAddress).text("");
+		});
+		
+		$("#clear").click(clear);
+		$("#check").click(check);
+	}
+	
+	
+	function clear(){
+		if(window.confirm("すべて削除します。よろしいですか？")){
+			$("table td:not(.defnum)").text("");
+		}else{
+			// do nothing
+		}
 	}
 
+	function check(){
+		//var $inputNunbers = $("table td:not(.defnum)");
+		var rows = [9];
+		var colomns = [9];
+		var squares = [9];
+		
+		//判定用の二次元配列を生成しておく
+		for(var idx=1; idx<10; idx++){
+			rows[idx] = (new Array(9)).fill(0);
+			colomns[idx] = (new Array(9)).fill(0);
+			squares[idx] = (new Array(9)).fill(0);
+		}
+		
+		$("table tr").each(function(i,tr){
+			$(tr).find("td").each(function(j,cell){
+				var cellValue = $(cell).text()+0;
+				//行
+				rows[i][cellValue] = 1;
+				//列
+				colomns[j][cellValue] = 1;
+				//3*3の四角
+				if(1<=i<=3 && 1<=j<=3){
+					squares[1][cellValue] = 1;
+				}
+				if(1<=i<=3 && 4<=j<=6){
+					squares[2][cellValue] = 1;
+				}
+				if(1<=i<=3 && 7<=j<=9){
+					squares[3][cellValue] = 1;
+				}
+				if(4<=i<=6 && 1<=j<=3){
+					squares[4][cellValue] = 1;
+				}
+				if(4<=i<=6 && 4<=j<=6){
+					squares[5][cellValue] = 1;
+				}
+				if(4<=i<=6 && 7<=j<=9){
+					squares[6][cellValue] = 1;
+				}
+				if(7<=i<=9 && 1<=j<=3){
+					squares[7][cellValue] = 1;
+				}
+				if(7<=i<=9 && 4<=j<=6){
+					squares[8][cellValue] = 1;
+				}
+				if(7<=i<=9 && 7<=j<=9){
+					squares[9][cellValue] = 1;
+				}
+			})
+			
+		});
+		for(var idx2=1; idx2<10; idx2++){
+			if($.inArray(0,rows[idx2]) == -1 && $.inArray(0,columns[idx2]) == -1 && $.inArray(0,suqares[idx2]) == -1){
+				successFlg = true;
+			}
+			if(successFlg){
+				alert("クリア！");
+				return true;//break
+			}else{
+				alert("違います");
+				return true;//break
+			}
+		}
+		
+	}
+	
 	//初期配置関数 一時的に固定配置にしてあります
 	function putDefaultNumber(){
 		$("#1").find(".1").text("5").addClass("defnum");
