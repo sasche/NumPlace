8022ed21c7efd3a74d830bbeceed60c98aa24f22
sasche <32791190+sasche@users.noreply.github.com>
2017-10-18 22:33:43 +0900

    test
    test

--- src/assets/js/NumberPlace.js
+++ src/assets/js/NumberPlace.js
@@ -4,7 +4,6 @@
 	var xAddress = null;
 	var yAddress = null;
 	var targetNum;
-	var successFlg = true;
 
 	function init() {
 		for(var i=1; i<10; i++){
@@ -13,6 +12,8 @@
 				$("#" + i).append("<td class=" + j + ">");
 			}
 		}
+		//$("#numtable").css("background-color","#ffffff");
+		//$("#1").find(".1").addClass("testing");
 
 		for(var i=1; i<10; i++){
 			$(".SelNum").append("<input type = button value=" + i + " class=numbutton>");
@@ -39,6 +40,7 @@
 					//console.log(xAddress + " " + yAddress);
 					$('td').css('background-color', 'transparent');
 					$(this).css("background-color","#7fffd4");
+					$(this).addClass('numIn');
 				}
 		);
 
@@ -48,92 +50,31 @@
 
 					if( xAddress != null && yAddress != null ){
 						$("#" + yAddress).find("." + xAddress).text(targetNum);
+						//xAddress = null;
+						//yAddress = null;
 					};
 				}
 		);
-		$("#del").click(function(){
-			$("#" + yAddress).find("." + xAddress).text("");
-		});
-		
-		$("#clear").click(clear);
-		$("#check").click(check);
-	}
-	
-	
-	function clear(){
-		if(window.confirm("すべて削除します。よろしいですか？")){
-			$("table td:not(.defnum)").text("");
-		}else{
-			// do nothing
-		}
-	}
 
-	function check(){
-		//var $inputNunbers = $("table td:not(.defnum)");
-		var rows = [9];
-		var columns = [9];
-		var squares = [9];
-		
-		//判定用の二次元配列を生成しておく
-		for(var idx=0; idx<9; idx++){
-			rows[idx] = (new Array()).fill(0);
-			columns[idx] = (new Array()).fill(0);
-			squares[idx] = (new Array()).fill(0);
-		}
-		
-		$("table tr").each(function(i,tr){
-			$(tr).find("td").each(function(j,cell){
-				var rowIdx = i+1;
-				var columnIdx = j+1
-				var cellValue = parseInt($(cell).text());
-				//行
-				rows[i][cellValue-1] = 1;
-				//列
-				columns[j][cellValue-1] = 1;
-				//3*3の四角
-				if(1<=rowIdx<=3 && 1<=columnIdx<=3){
-					squares[0][cellValue] = 1;
+		$('body').on('click','.DelBtn',function(){
+					if( xAddress != null && yAddress != null){
+						$("#" + yAddress).find("." + xAddress).text('');
+					};
 				}
-				if(1<=rowIdx<=3 && 4<=columnIdx<=6){
-					squares[1][cellValue] = 1;
+		);
+
+		$('body').on('click','.DelAllBtn',function(){
+				for(var i=1;i<10;i++){
+					for(var j=1;j<10;j++){
+						if($("#" + i).find("." + j).hasClass('numIn')){
+							$("#" + i).find("." + j).text('');
+						}
+					}
 				}
-				if(1<=rowIdx<=3 && 7<=columnIdx<=9){
-					squares[2][cellValue] = 1;
-				}
-				if(4<=rowIdx<=6 && 1<=columnIdx<=3){
-					squares[3][cellValue] = 1;
-				}
-				if(4<=rowIdx<=6 && 4<=columnIdx<=6){
-					squares[4][cellValue] = 1;
-				}
-				if(4<=rowIdx<=6 && 7<=columnIdx<=9){
-					squares[5][cellValue] = 1;
-				}
-				if(7<=rowIdx<=9 && 1<=columnIdx<=3){
-					squares[6][cellValue] = 1;
-				}
-				if(7<=rowIdx<=9 && 4<=columnIdx<=6){
-					squares[7][cellValue] = 1;
-				}
-				if(7<=rowIdx<=9 && 7<=columnIdx<=9){
-					squares[8][cellValue] = 1;
-				}
-			})
-			
-		});
-		for(var idx2=1; idx2<10; idx2++){
-			if($.inArray(0,rows[idx2]) != -1 && $.inArray(0,columns[idx2]) != -1 && $.inArray(0,squares[idx2]) != -1){
-				successFlg = false;
 			}
-
-		}
-		if(successFlg){
-			alert("クリア！");
-		}else{
-			alert("違います");
-		}
+		);
 	}
-	
+
 	//初期配置関数 一時的に固定配置にしてあります
 	function putDefaultNumber(){
 		$("#1").find(".1").text("5").addClass("defnum");
@@ -165,60 +106,5 @@
 		$("#8").find(".9").text("5").addClass("defnum");
 		$("#9").find(".5").text("8").addClass("defnum");
 		$("#9").find(".8").text("7").addClass("defnum");
-		$("#9").find(".9").text("9").addClass("defnum");
-		
-		//デバッグ用解答
-		$("#1").find(".3").text("4").addClass("defnum");
-		$("#1").find(".3").text("4").addClass("defnum");
-		$("#1").find(".4").text("6").addClass("defnum");
-		$("#1").find(".6").text("8").addClass("defnum");
-		$("#1").find(".7").text("9").addClass("defnum");
-		$("#1").find(".8").text("1").addClass("defnum");
-		$("#1").find(".9").text("2").addClass("defnum");
-		$("#2").find(".2").text("7").addClass("defnum");
-		$("#2").find(".3").text("2").addClass("defnum");
-		$("#2").find(".7").text("3").addClass("defnum");
-		$("#2").find(".8").text("4").addClass("defnum");
-		$("#2").find(".9").text("8").addClass("defnum");
-		$("#3").find(".1").text("1").addClass("defnum");
-		$("#3").find(".4").text("3").addClass("defnum");
-		$("#3").find(".5").text("4").addClass("defnum");
-		$("#3").find(".6").text("2").addClass("defnum");
-		$("#3").find(".7").text("2").addClass("defnum");
-		$("#3").find(".9").text("7").addClass("defnum");
-		$("#4").find(".2").text("5").addClass("defnum");
-		$("#4").find(".3").text("9").addClass("defnum");
-		$("#4").find(".4").text("7").addClass("defnum");
-		$("#4").find(".6").text("1").addClass("defnum");
-		$("#4").find(".7").text("4").addClass("defnum");
-		$("#4").find(".8").text("2").addClass("defnum");
-		$("#5").find(".2").text("2").addClass("defnum");
-		$("#5").find(".3").text("6").addClass("defnum");
-		$("#5").find(".5").text("5").addClass("defnum");
-		$("#5").find(".7").text("7").addClass("defnum");
-		$("#5").find(".8").text("9").addClass("defnum");
-		$("#6").find(".2").text("1").addClass("defnum");
-		$("#6").find(".3").text("3").addClass("defnum");
-		$("#6").find(".4").text("9").addClass("defnum");
-		$("#6").find(".6").text("4").addClass("defnum");
-		$("#6").find(".7").text("8").addClass("defnum");
-		$("#6").find(".8").text("5").addClass("defnum");
-		$("#7").find(".1").text("9").addClass("defnum");
-		$("#7").find(".3").text("6").addClass("defnum");
-		$("#7").find(".4").text("1").addClass("defnum");
-		$("#7").find(".5").text("5").addClass("defnum");
-		$("#7").find(".6").text("3").addClass("defnum");
-		$("#7").find(".9").text("7").addClass("defnum");
-		$("#8").find(".1").text("2").addClass("defnum");
-		$("#8").find(".2").text("8").addClass("defnum");
-		$("#8").find(".3").text("7").addClass("defnum");
-		$("#8").find(".7").text("6").addClass("defnum");
-		$("#8").find(".8").text("3").addClass("defnum");
-		$("#9").find(".1").text("3").addClass("defnum");
-		$("#9").find(".2").text("4").addClass("defnum");
-		$("#9").find(".3").text("5").addClass("defnum");
-		$("#9").find(".4").text("2").addClass("defnum");
-		$("#9").find(".6").text("6").addClass("defnum");
-		$("#9").find(".7").text("1").addClass("defnum");
 		$("#9").find(".9").text("9").addClass("defnum");
 	}
